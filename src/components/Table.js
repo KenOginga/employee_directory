ad7e17339dda855933b613f50a4da9a920639fd9
KenOginga <72943649+KenOginga@users.noreply.github.com>
2021-01-13 23:20:49 -0600

    sorting functions added and invoked in the table component.

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,27 +5,43 @@ import "./style.css";
 
 
 class Table extends Component {
+    // Setting component's initial state
     state = {
         employees: [],
-        sortedEmployees: [],
-        search: ""
+        filteredEmployees: [],
+        search: "",
+        order: ""
     }
 
     componentDidMount() {
         API.getEmployees()
             .then(employeesData => this.setState({ employees: employeesData.data.results }))
-            .then(employees => this.setState({ sortedEmployees: this.state.employees }))
-            .then(res => console.log(this.state.employees))
-    }
+            .then(employees => this.setState({ filteredEmployees: this.state.employees }))
+    };
     
     handleInputChange = (event) => {
         const employeeName = event.target.value;
         const searchedEmployeeArray = this.state.employees.filter(employee => employee.name.first.includes(employeeName) || employee.name.last.includes(employeeName));
-        this.setState({ sortedEmployees: searchedEmployeeArray})
-    }
-    // sortByName = ()=>{
-    //     const sort = sfsf
-    // }
+        this.setState({ filteredEmployees: searchedEmployeeArray})
+    };
+
+    sortByName = ()=>{
+        const sortingList = this.state.filteredEmployees;
+        if (this.state.order === "asc"){
+            const sortedNames = sortingList.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1);
+            this.setState({
+                filteredEmployees: sortedNames,
+                order: "desc"
+            });
+        } else {
+            const sortedNames = sortingList.sort((a, b) => (a.name.first > b.name.first) ? -1 : 1);
+            this.setState({
+                filteredEmployees: sortedNames,
+                order: "asc"
+            })
+        }
+    };
+
     render() {
 
         return (
@@ -34,7 +50,7 @@ class Table extends Component {
                     <thead>
                         <tr>
                             <th scope="col-2"></th>
-                            <th scope="col">Name</th>
+                            <th scope="col" onClick={this.sortByName}>Name</th>
                             <th scope="col">Phone</th>
                             <th scope="col">Email</th>
                             <th scope="col">Date of Birth</th>
